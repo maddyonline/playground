7204530a9af82a2a688d6cd3bbcb03214359a770
Madhav Jha <madhavjha@gmail.com>
2021-04-03 14:28:58 -0700

    initial playground with store support

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import store from "store2";
 
 function App() {
+  const [code, setCode] = React.useState("<strong>Hello World!</strong>");
+  const [savedCode, setSavedCode] = React.useState("");
+  window.store = store;
+  store.set("key", { name: "maddy" });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex" }}>
+      <div>
+        <LiveProvider code={code}>
+          <LiveEditor
+            value={code}
+            onValueChange={(code) => setCode(code)}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 12,
+            }}
+          />
+          <LiveError />
+          <LivePreview />
+        </LiveProvider>
+      </div>
+      <div>
+        <pre>{savedCode}</pre>
+        <button onClick={() => setSavedCode(store.get("code") ?? "")}>
+          Recall from store
+        </button>
+        <button onClick={() => setCode(savedCode)}>Load editor</button>
+        <button onClick={() => store("code", code)}>Save in store</button>
+        <button onClick={() => store(false)}>Clear store</button>
+      </div>
     </div>
   );
 }
